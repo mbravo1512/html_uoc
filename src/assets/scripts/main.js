dc1da60b03f4429735a44afde60f0541a1865770
Manuel Geovanny Bravo Pico <mbravo1512@uoc.edu>
2020-12-04 16:35:31 -0500

    Listo pac2

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -14,23 +14,4 @@
   console.log('Hello, UOC!');
 } )();
 
-$('.progress-bar').each(function() {
-  "use strict";
-  	var $bar = $(this);
-    var progress = setInterval(function() {
-      
-      var currWidth = parseInt($bar.attr('aria-valuenow'));
-      var maxWidth = parseInt($bar.attr('aria-valuemax'));
-      
-	  //update the progress
-        $bar.width(currWidth+'%');
-        $bar.attr('aria-valuenow',currWidth+10);
-      
-      //clear timer when max is reach
-      if (currWidth >= maxWidth){
-        clearInterval(progress);
-      }
-      
-    }, 500);
-  
-});
+
